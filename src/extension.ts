f3744a5c6cdc5b4f23b6c92d385b955bf5121f6e
Andy Friesen <andy.friesen@gmail.com>
2023-07-23 16:49:30 -0700

    Stuff.

--- src/extension.ts
+++ src/extension.ts
@@ -6,11 +6,15 @@ import * as os from 'os';
 import * as vscode from 'vscode';
 
 let platformStatusBarItem: vscode.StatusBarItem;
-const clickedStatusBarCommandId = 'tim-buck2.selectPlatform';
-const buildCommandId = 'tim-buck2.build';
+const clickedSelectPlatformCommandId = 'tim-buck2.selectPlatform';
+let validPlatforms: string[] = [];
+let currentPlatform = '';
 
+let targetStatusBarItem: vscode.StatusBarItem;
+const clickedTargetCommandId = 'tim-buck2.selectTarget';
+const buildCommandId = 'tim-buck2.build';
 let validTargets: string[] = [];
-let currentTarget: string = '';
+let currentTarget = '';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -23,22 +27,22 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('tim-buck2.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from tim-buck2!');
-    });
 
-    currentTarget = "//platforms:platforhms";
-
+    let disposable = vscode.commands.registerCommand(buildCommandId, build);
     context.subscriptions.push(disposable);
 
-    context.subscriptions.push(vscode.commands.registerCommand(clickedStatusBarCommandId, onClickedStatusBarCommand));
+    currentPlatform = "//platforms:platforhms";
+    currentTarget = ":";
 
     platformStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-    platformStatusBarItem.command = clickedStatusBarCommandId;
-
+    platformStatusBarItem.command = clickedSelectPlatformCommandId;
     context.subscriptions.push(platformStatusBarItem);
+    context.subscriptions.push(vscode.commands.registerCommand(clickedSelectPlatformCommandId, onClickedPlatform));
+
+    targetStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    targetStatusBarItem.command = clickedTargetCommandId;
+    context.subscriptions.push(targetStatusBarItem);
+    context.subscriptions.push(vscode.commands.registerCommand(clickedTargetCommandId, onClickedTarget));
 
     context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBars));
     context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBars));
@@ -46,38 +50,99 @@ export function activate(context: vscode.ExtensionContext) {
     updateStatusBars();
 }
 
-async function onClickedStatusBarCommand() {
+async function build() {
+    
+}
+
+async function onClickedPlatform() {
     const platforms = await getPlatforms();
 
-    vscode.window.showQuickPick(platforms).then((tgt) => {
-        if (tgt) {
-            currentTarget = tgt;
+    vscode.window.showQuickPick(platforms).then((newPlatform) => {
+        if (newPlatform) {
+            currentPlatform = newPlatform;
+        }
+        updateStatusBars();
+    });
+}
+
+async function onClickedTarget() {
+    const targets = await getTargets();
+
+    vscode.window.showQuickPick(targets).then((newTarget) => {
+        if (newTarget) {
+            currentTarget = newTarget;
         }
         updateStatusBars();
     });
 }
 
 function updateStatusBars() {
-    if (currentTarget) {
-        platformStatusBarItem.text = currentTarget;
-        platformStatusBarItem.show();
-    } else {
-        platformStatusBarItem.hide();
-    }
+    platformStatusBarItem.text = currentPlatform ? `Platform: ${currentPlatform}` : "Platform...";
+    platformStatusBarItem.show();
+
+    targetStatusBarItem.text = currentTarget ? `Target: ${currentTarget}` : "Target...";
+    targetStatusBarItem.show();
 }
 
+function splitLines(s: string): string[] {
+    return s.split("\n").filter(s => s && s.length > 0).map(s => s.trim());
+}
+
+async function getTargets(): Promise<string[]> {
+    let stdout = await runBuck(["targets", ":"]);
+    return splitLines(stdout);
+}
+
+// TODO: Only do this on startup or when we sense a change to .buckconfig or .buckconfig.local
 function getPlatforms(): Promise<string[]> {
     return new Promise((resolve, reject) => {
         const conf = vscode.workspace.getConfiguration('tim-buck2');
-        
+
         const buck2Path = conf.get('buck2Path') as string;
         assert(typeof buck2Path === 'string');
-        
+
         const targetMask = conf.get('platformTargetMask') as string;
         assert(typeof targetMask === 'string');
-        
+
+        // TODO: What if there are multiple folders?
         const workspaceUri = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length ? vscode.workspace.workspaceFolders[0].uri : null;
-        
+
+        if (!workspaceUri) {
+            reject('No workspace URI');
+            return;
+        }
+
+        const options = {
+            // TODO: There may be more than one folder
+            cwd: workspaceUri.fsPath
+        };
+
+        execFile(buck2Path, ['targets', targetMask], options, (err, stdout, stderr) => {
+            if (err) {
+                console.error('Failed to get list of platforms');
+                console.error(err);
+                console.warn(stderr);
+                reject(err);
+                return;
+            }
+
+            validPlatforms = stdout.split('\n').filter(s => s && s.length > 0);
+
+            resolve(validPlatforms);
+        });
+    });
+}
+
+function runBuck(args: string[]): Promise<string> {
+    return new Promise((resolve, reject) => {
+        const conf = vscode.workspace.getConfiguration('tim-buck2');
+
+        const buck2Path = conf.get('buck2Path') as string;
+        assert(typeof buck2Path === 'string');
+
+        // TODO: What if there are multiple folders?
+        const workspaceUri = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length ? vscode.workspace.workspaceFolders[0].uri : null;
+
         if (!workspaceUri) {
             reject('No workspace URI');
             return;
@@ -88,18 +153,16 @@ function getPlatforms(): Promise<string[]> {
             cwd: workspaceUri.fsPath
         };
         
-        execFile(buck2Path, ['targets', targetMask], options, (err, stdout, stderr) => {
+        execFile(buck2Path, args, options, (err, stdout, stderr) => {
             if (err) {
-                console.error('Failed to get list of platforms');
+                console.error('Buck2 command failed:', args);
                 console.error(err);
                 console.warn(stderr);
                 reject(err);
                 return;
             }
-        
-            validTargets = stdout.split('\n').filter(s => s && s.length > 0);
 
-            resolve(validTargets);
+            resolve(stdout);
         });
     });
 }
@@ -107,4 +170,5 @@ function getPlatforms(): Promise<string[]> {
 // This method is called when your extension is deactivated
 export function deactivate() {
     platformStatusBarItem.hide();
+    targetStatusBarItem.hide();
 }
