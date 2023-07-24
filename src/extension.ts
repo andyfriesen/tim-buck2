e678f209e5da4d29343bc97fede217ce5375f499
Andy Friesen <andy.friesen@gmail.com>
2023-07-23 19:03:44 -0700

    More stuff.

--- src/extension.ts
+++ src/extension.ts
@@ -1,20 +1,26 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { execFile } from "node:child_process";
+import { ChildProcess, execFile } from "node:child_process";
 import { assert } from "node:console";
 import * as os from 'os';
 import * as vscode from 'vscode';
 
 let platformStatusBarItem: vscode.StatusBarItem;
 const clickedSelectPlatformCommandId = 'tim-buck2.selectPlatform';
-let validPlatforms: string[] = [];
 let currentPlatform = '';
 
 let targetStatusBarItem: vscode.StatusBarItem;
 const clickedTargetCommandId = 'tim-buck2.selectTarget';
-const buildCommandId = 'tim-buck2.build';
-let validTargets: string[] = [];
 let currentTarget = '';
+
+let stopBuildButton: vscode.StatusBarItem;
+const stopBuildCommandId = 'tim-buck2.stopBuild';
+let buildIsRunning = false;
+let buildProcess: ChildProcess | undefined;
+
+const cleanCommandId = 'tim-buck2.clean';
+
+const buildCommandId = 'tim-buck2.build';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -28,30 +34,79 @@ export function activate(context: vscode.ExtensionContext) {
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
 
-    let disposable = vscode.commands.registerCommand(buildCommandId, build);
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.commands.registerCommand(buildCommandId, build));
+    context.subscriptions.push(vscode.commands.registerCommand(cleanCommandId, clean));
+    context.subscriptions.push(vscode.commands.registerCommand(stopBuildCommandId, stopBuild));
 
-    currentPlatform = "//platforms:platforhms";
-    currentTarget = ":";
+    // TODO scoop up from .buckconfig
+    currentPlatform = "root//platforms:release-nosan";
+    currentTarget = ":Luau.UnitTest";
 
-    platformStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    platformStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
     platformStatusBarItem.command = clickedSelectPlatformCommandId;
+    platformStatusBarItem.tooltip = 'Select Build Platform';
     context.subscriptions.push(platformStatusBarItem);
     context.subscriptions.push(vscode.commands.registerCommand(clickedSelectPlatformCommandId, onClickedPlatform));
 
-    targetStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+    targetStatusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 2);
     targetStatusBarItem.command = clickedTargetCommandId;
+    targetStatusBarItem.tooltip = 'Select Build Target';
     context.subscriptions.push(targetStatusBarItem);
     context.subscriptions.push(vscode.commands.registerCommand(clickedTargetCommandId, onClickedTarget));
+
+    stopBuildButton = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 1);
+    // stopBuildButton.text = '$(loading~spin) Build';
+    stopBuildButton.text = '$(settings-gear) Build';
+    stopBuildButton.command = stopBuildCommandId;
 
     context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(updateStatusBars));
     context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(updateStatusBars));
 
     updateStatusBars();
+    updateBuildButton();
 }
 
-async function build() {
-    
+function build() {
+    if (buildIsRunning) {
+        vscode.window.showInformationMessage('A build is already running');
+        return;
+    }
+
+    const conf = vscode.workspace.getConfiguration('tim-buck2');
+
+    const buck2Path = conf.get('buck2Path') as string;
+    assert(typeof buck2Path === 'string');
+
+    const cmd = ['build'];
+    if (currentPlatform.length > 0) {
+        cmd.push('--target-platforms');
+        cmd.push(currentPlatform);
+    }
+
+    cmd.push(currentTarget);
+
+    buildIsRunning = true;
+    buildProcess = execFile(buck2Path, cmd, { cwd: getWorkspaceRoot() }, (err, stdout, stderr) => {
+        buildIsRunning = false;
+        buildProcess = undefined;
+        updateBuildButton();
+    });
+
+    updateBuildButton();
+}
+
+function stopBuild() {
+    if (!buildProcess) {
+        build();
+    } else {
+        buildProcess.kill();
+    }
+
+    updateBuildButton();
+}
+
+function clean() {
+    runBuck(['clean']);
 }
 
 async function onClickedPlatform() {
@@ -76,6 +131,16 @@ async function onClickedTarget() {
     });
 }
 
+function updateBuildButton() {
+    if (buildProcess) {
+        stopBuildButton.text = '$(loading~spin) Cancel Build';
+    } else {
+        stopBuildButton.text = "$(settings-gear) Build";
+    }
+
+    stopBuildButton.show();
+}
+
 function updateStatusBars() {
     platformStatusBarItem.text = currentPlatform ? `Platform: ${currentPlatform}` : "Platform...";
     platformStatusBarItem.show();
@@ -93,44 +158,23 @@ async function getTargets(): Promise<string[]> {
     return splitLines(stdout);
 }
 
-// TODO: Only do this on startup or when we sense a change to .buckconfig or .buckconfig.local
-function getPlatforms(): Promise<string[]> {
-    return new Promise((resolve, reject) => {
-        const conf = vscode.workspace.getConfiguration('tim-buck2');
+async function getPlatforms(): Promise<string[]> {
+    const conf = vscode.workspace.getConfiguration('tim-buck2');
 
-        const buck2Path = conf.get('buck2Path') as string;
-        assert(typeof buck2Path === 'string');
+    const targetMask = conf.get('platformTargetMask') as string;
+    assert(typeof targetMask === 'string');
 
-        const targetMask = conf.get('platformTargetMask') as string;
-        assert(typeof targetMask === 'string');
+    let stdout = await runBuck(["targets", targetMask]);
+    return splitLines(stdout);
+}
 
-        // TODO: What if there are multiple folders?
-        const workspaceUri = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length ? vscode.workspace.workspaceFolders[0].uri : null;
+function getWorkspaceRoot() {
+    // TODO: What if there are multiple folders?
+    const workspaceUri = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length ? vscode.workspace.workspaceFolders[0].uri : null;
 
-        if (!workspaceUri) {
-            reject('No workspace URI');
-            return;
-        }
+    assert(workspaceUri);
 
-        const options = {
-            // TODO: There may be more than one folder
-            cwd: workspaceUri.fsPath
-        };
-
-        execFile(buck2Path, ['targets', targetMask], options, (err, stdout, stderr) => {
-            if (err) {
-                console.error('Failed to get list of platforms');
-                console.error(err);
-                console.warn(stderr);
-                reject(err);
-                return;
-            }
-
-            validPlatforms = stdout.split('\n').filter(s => s && s.length > 0);
-
-            resolve(validPlatforms);
-        });
-    });
+    return workspaceUri?.fsPath;
 }
 
 function runBuck(args: string[]): Promise<string> {
@@ -140,19 +184,10 @@ function runBuck(args: string[]): Promise<string> {
         const buck2Path = conf.get('buck2Path') as string;
         assert(typeof buck2Path === 'string');
 
-        // TODO: What if there are multiple folders?
-        const workspaceUri = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length ? vscode.workspace.workspaceFolders[0].uri : null;
-
-        if (!workspaceUri) {
-            reject('No workspace URI');
-            return;
-        }
-
         const options = {
-            // TODO: There may be more than one folder
-            cwd: workspaceUri.fsPath
+            cwd: getWorkspaceRoot()
         };
-        
+
         execFile(buck2Path, args, options, (err, stdout, stderr) => {
             if (err) {
                 console.error('Buck2 command failed:', args);
